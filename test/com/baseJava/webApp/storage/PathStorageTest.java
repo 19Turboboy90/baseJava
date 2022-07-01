87aa28695289d79b2b1258726ddb967683b10ae0
Илья <90090224+19Turboboy90@users.noreply.github.com>
2022-07-01 17:15:55 +0300

    HW10:Refactoring HW

--- test/com/baseJava/webApp/storage/PathStorageTest.java
+++ test/com/baseJava/webApp/storage/PathStorageTest.java
@@ -5,6 +5,6 @@ import com.baseJava.webApp.storage.serializer.ObjectStreamStorage;
 public class PathStorageTest extends AbstractStorageTest{
 
     public PathStorageTest() {
-        super(new PathStorage("E:\\Программирование\\JAVA\\Java_Курс\\basejava\\storage", new ObjectStreamStorage()));
+        super(new PathStorage(STORAGE_DIR.getAbsolutePath(), new ObjectStreamStorage()));
     }
 }
