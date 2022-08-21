7c1863c8122daab5d78d0a60f60490d55dd45623
Илья <90090224+19Turboboy90@users.noreply.github.com>
2022-08-21 20:40:37 +0300

    HW15: 1.1 - addition in the resumeServlet class

--- src/ru/baseJava/webApp/web/resumeServlet.java
+++ src/ru/baseJava/webApp/web/resumeServlet.java
@@ -9,7 +9,11 @@ import java.io.IOException;
 public class resumeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("Hello Servlet");
+        req.setCharacterEncoding("UTF-8");
+        resp.setCharacterEncoding("UTF-8");
+        resp.setContentType("text/html; charset=UTF-8");
+        String name = req.getParameter("name");
+        resp.getWriter().write(name == null ? "Hello Servlet" : "Hello " + name + '!');
     }
 
     @Override
