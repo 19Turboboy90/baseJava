e8ce0cb2ab9ffe0fcf5a5053a3a55c9c2d0685fd
Илья <90090224+19Turboboy90@users.noreply.github.com>
2022-08-23 16:49:21 +0300

    HW15: 1.2 - added table resume

--- src/ru/baseJava/webApp/web/resumeServlet.java
+++ src/ru/baseJava/webApp/web/resumeServlet.java
@@ -1,11 +1,13 @@
 package ru.baseJava.webApp.web;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@WebServlet("/resume")
 public class resumeServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -13,7 +15,25 @@ public class resumeServlet extends HttpServlet {
         resp.setCharacterEncoding("UTF-8");
         resp.setContentType("text/html; charset=UTF-8");
         String name = req.getParameter("name");
-        resp.getWriter().write(name == null ? "Hello Servlet" : "Hello " + name + '!');
+//        req.getRequestDispatcher("table.html").forward(req,resp);
+        String html = "<html>\n" +
+                "<style>\n" +
+                "    table, th, td {\n" +
+                "        width: 100px;\n" +
+                "        height: 50px;\n" +
+                "        border: 1px solid black;\n" +
+                "    }\n" +
+                "</style>\n" +
+                "<body>\n" +
+                "<table style=\"width:100%\">\n" +
+                "    <tr>\n" +
+                "        <th>id</th>\n" +
+                "        <th>full_name</th>\n" +
+                "    </tr>\n" +
+                "</table>\n" +
+                "</body>\n" +
+                "</html>\n";
+        resp.getWriter().write(name == null ? html : "Hello " + name + '!');
     }
 
     @Override
